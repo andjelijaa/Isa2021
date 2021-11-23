24ddb90464cbb9c6bf0ca2253792e4925fc9a012
andjelijaa <andjelija.djordjevic1997@gmail.com>
2021-11-23 12:29:48 +0100

    Formiranje fronta

---
+++ frontend/helloworld/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import Vue from 'vue'
+
+createApp(App).mount('#app')
+
+new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
+  
+
+//Vue.config.productionTip = false
