bff12dc4570b8f0dd4ab35094e61ab2ef90dbee9
andjelijaa <andjelija.djordjevic1997@gmail.com>
2021-12-04 23:58:16 +0100

    Dodavanje User-a i security-a potrebnih za autentifikaciju

---
+++ ISA2021/src/main/java/com/example/ISA2021/Services/MyUserDetailService.java
@@ -0,0 +1,12 @@
+package com.example.ISA2021.Services;
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailServece implements UserDetailsService {
+    
+}
