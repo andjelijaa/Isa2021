01439d55f1f863a0a2f67f35896dce25a0910626
andjelijaa <andjelija.djordjevic1997@gmail.com>
2021-12-05 22:07:23 +0100

    Sredjivanje baze

--- ISA2021/src/main/java/com/example/ISA2021/Isa2021Application.java
+++ ISA2021/src/main/java/com/example/ISA2021/Isa2021Application.java
@@ -1,10 +1,7 @@
 package com.example.ISA2021;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
