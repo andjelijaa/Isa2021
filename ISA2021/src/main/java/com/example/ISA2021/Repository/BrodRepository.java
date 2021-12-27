a47aef1abb407f34eca5524506413bc60cc02a18
andjelijaa <62313553+andjelijaa@users.noreply.github.com>
2021-12-27 21:04:48 +0100

    Delete ISA2021 directory

--- ISA2021/src/main/java/com/example/ISA2021/Repository/BrodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ISA2021.Repository;
-
-import com.example.ISA2021.Models.Brod;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BrodRepository extends JpaRepository<Brod, Long> {
-}
