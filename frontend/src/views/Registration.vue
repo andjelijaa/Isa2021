e7c37678501090abc48d6007e03b089778ba98a0
andjelijaa <andjelija.djordjevic1997@gmail.com>
2022-04-15 13:57:01 +0200

    Dodavanje fronta za login i registration

--- frontend/src/views/Registration.vue
+++ frontend/src/views/Registration.vue
@@ -1,7 +1,7 @@
 <template>
-<div>
-  <b-form @submit="onSubmit" @reset="onReset">
-        <b-form-group
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-form-group
         label="Email address:"
         >
         <b-form-input
@@ -23,19 +23,30 @@
         ></b-form-input>
       </b-form-group>
 
-<b-button variant="primary">Login</b-button>      </b-form>
-      </div>
-      </template>
+      <b-form-group
+        label="Address:"
+        >
+        <b-form-input
+          v-model="form.address"
+          placeholder="Address"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button variant="primary">Sign up</b-button>
+    </b-form>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Login",
+  name: "Registration",
   data() {
       return {
         form: {
           email: '',
-          password: ''
+          password: '',
+          address: ''
         }
       }
   },
@@ -46,10 +57,10 @@ name: "Login",
       },
       onReset(event) {
         event.preventDefault()
-        // Reset our form values
         this.form.email = ''
         this.form.password = ''
+        this.form.address = ''
       }
     }
-};
+}
 </script>
