2c05b98e513f9c3c30a4b09d4f76b47ffb958c9a
natasa98 <73998322+natasa98@users.noreply.github.com>
2022-04-26 16:12:57 +0200

    Dodavanje homeContoller sa Dto modelima

--- backend/src/main/java/com/example/backend/controllers/HomeController.java
+++ backend/src/main/java/com/example/backend/controllers/HomeController.java
@@ -5,6 +5,9 @@ import com.example.backend.models.Brod;
 import com.example.backend.models.Instruktor;
 import com.example.backend.models.Vikendica;
 import com.example.backend.models.local.HomeModel;
+import com.example.backend.models.response.BrodDto;
+import com.example.backend.models.response.InstruktorDto;
+import com.example.backend.models.response.VikendicaDto;
 import com.example.backend.repository.BrodRepository;
 import com.example.backend.repository.InstruktorRepository;
 import com.example.backend.repository.VikendicaRepository;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RestController("/")
 public class HomeController {
@@ -24,13 +29,48 @@ public class HomeController {
     @Autowired
     InstruktorRepository instruktorRepository;
 
-    @GetMapping("/")
-    public String home(){
-//        kupe se podatci iz baze o broovima vikendicama i instruktorima
+    @GetMapping("/getAll")
+    public HomeModel home(){
         List<Brod> brodovi = brodRepository.findAll();
         List<Instruktor> instruktori = instruktorRepository.findAll();
         List<Vikendica> vikendice = vikendicaRepository.findAll();
         HomeModel returnModel = new HomeModel(instruktori, brodovi, vikendice);
-        return "home";
+        return returnModel;
+}
+
+    @GetMapping("/getVikendice")
+    public List<VikendicaDto> vikendice(){
+        List<Vikendica> vikendice = vikendicaRepository.findAll();
+
+        List<VikendicaDto> returnModel = vikendice
+                .stream()
+                .map(vikendica -> new VikendicaDto(vikendica))
+                .collect(Collectors.toList());
+
+        return returnModel;
+    }
+
+    @GetMapping("/getInstruktori")
+    public List<InstruktorDto> instruktori(){
+        List<Instruktor> instruktori = instruktorRepository.findAll();
+
+        List<InstruktorDto> returnModel = instruktori
+                .stream()
+                .map(instruktor -> new InstruktorDto(instruktor))
+                .collect(Collectors.toList());
+
+        return returnModel;
+    }
+
+    @GetMapping("/getBrodovi")
+    public List<BrodDto> brodovi(){
+        List<Brod> brodovi = brodRepository.findAll();
+
+        List<BrodDto> returnModel = brodovi
+                .stream()
+                .map(brod -> new BrodDto(brod))
+                .collect(Collectors.toList());
+
+        return returnModel;
     }
 }
--- backend/src/main/java/com/example/backend/models/Vikendica.java
+++ backend/src/main/java/com/example/backend/models/Vikendica.java
@@ -10,25 +10,36 @@ public class Vikendica {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "vikendicaId")
     private int vikendicaId;
+
     @Column(name = "naziv")
     private String naziv;
+
     @Column(name = "adresa")
     private String adresa;
+
     @Column(name = "opis")
     private String opis;
+
     @Column(name = "prosecna_ocena")
     private float prosecna_ocena;
+
     @Column(name = "slike")
     @ElementCollection(targetClass=Integer.class)
     private List<String> slike;
+
     @Column(name = "broj_soba")
     private int broj_soba;
+
     @Column(name = "broj_kreveta")
     private int broj_kreveta;
+
     @Column(name = "pravila_ponasanja")
     private String pravila_ponasanja;
-    //@Column(name="cenovnik")
-    //private Cenovnik cenovnik;
+
+
+    @Column(name="cenovnik")
+    private Cenovnik cenovnik;
+
     @ManyToOne
     @JoinColumn(name = "vikendicaId",insertable = false, updatable = false)
     private Vlasnik vlasnik;
@@ -41,14 +52,6 @@ public class Vikendica {
         this.vlasnik = vlasnik;
     }
 
-     /*public Cenovnik getCenovnik() {
-         return cenovnik;
-     }
-
-     public void setCenovnik(Cenovnik cenovnik) {
-         this.cenovnik = cenovnik;
-     }
-*/
          public List<String> getSlike() {
              return slike;
          }
@@ -122,5 +125,14 @@ public class Vikendica {
 
     public void setProsecna_ocena(float prosecna_ocena) {
         this.prosecna_ocena = prosecna_ocena;
+    }
+
+
+    public Cenovnik getCenovnik() {
+        return cenovnik;
+    }
+
+   public void setCenovnik(Cenovnik cenovnik) {
+        this.cenovnik = cenovnik;
     }
 }
