5d76900923431a29a3f65fd3741d5aa40cd47a0c
andjelijaa <andjelija.djordjevic1997@gmail.com>
2022-06-09 15:38:46 +0200

    Sredjivanje BrodController-a i  dodavanje u response

--- backend/src/main/java/com/example/backend/controllers/HomeController.java
+++ backend/src/main/java/com/example/backend/controllers/HomeController.java
@@ -2,22 +2,12 @@ package com.example.backend.controllers;
 
 
 import com.example.backend.models.*;
-import com.example.backend.models.local.HomeModel;
-import com.example.backend.models.response.BrodDto;
 import com.example.backend.models.response.IndexDto;
-import com.example.backend.models.response.InstruktorDto;
-import com.example.backend.models.response.VikendicaDto;
-import com.example.backend.repository.BrodRepository;
-import com.example.backend.repository.InstruktorRepository;
-import com.example.backend.repository.VikendicaRepository;
 import com.example.backend.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @RestController("/api")
