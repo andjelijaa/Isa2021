44c73394c13d7853116a06cedfdd31fd01b7957b
andjelijaa <andjelija.djordjevic1997@gmail.com>
2022-08-14 12:48:49 +0200

    Formiranje BrodService i koriscenje u controlleru

--- backend/src/main/java/com/example/backend/controllers/BrodController.java
+++ backend/src/main/java/com/example/backend/controllers/BrodController.java
@@ -1,60 +1,32 @@
 package com.example.backend.controllers;
 
 import com.example.backend.models.Brod;
-import com.example.backend.models.Role;
-import com.example.backend.models.User;
 import com.example.backend.models.response.GetBrodDTO;
-import com.example.backend.repository.BrodRepository;
-import com.example.backend.services.UserService;
+import com.example.backend.services.BrodService;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController("/api/brod")
 public class BrodController {
-    private final BrodRepository brodRepository;
-    private final UserService userService;
 
-    public BrodController(BrodRepository brodRepository, UserService userService) {
-        this.brodRepository = brodRepository;
-        this.userService = userService;
+    private final BrodService brodService;
+
+    public BrodController(BrodService brodService) {
+        this.brodService = brodService;
+
     }
 
     @RequestMapping(value = "/getAllBrodovi", method = RequestMethod.GET)
     public List<Brod> getAll(Principal principal) throws Exception {
-        User user = userService.getActivatedUserFromPrincipal(principal);
-        if(user == null){
-            throw new Exception("User not found");
-        }
-        List<Brod> brodovi = brodRepository.findAll();
-        return brodovi;
+        return brodService.getAllBrodovi(principal);
     }
 
     @GetMapping("/{brodId}")
     public GetBrodDTO getBrod(Principal principal,
                               @PathVariable(name = "brodId") Long brodId) throws Exception {
-        User user = userService.getActivatedUserFromPrincipal(principal);
-        if(user == null){
-            throw new Exception("User not found");
-        }
-        Optional<Brod> brod = brodRepository.findById(brodId);
-        if(brod.isEmpty()){
-            throw new Exception("Brod not found");
-        }
-        GetBrodDTO response = null;
-        if(Role.ROLE_VLASNIK_BRODA == user.getRole() && brod.get().getVlasnik().getUsername().equals(user.getUsername())){
-            response = new GetBrodDTO(
-                    brod.get(),
-                    true
-            );
-            return response;
-        }
-        response = new GetBrodDTO(
-                brod.get(),
-                false
-        );
-        return response;
+        return brodService.getBrodByBrodId(principal, brodId);
     }
 }
