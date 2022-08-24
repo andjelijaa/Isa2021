5d418973568e2ca466d110008cfb678bea0fb37d
andjelijaa <andjelija.djordjevic1997@gmail.com>
2022-08-24 23:38:39 +0200

    Dodavanje funkcije editovanja na back i front

--- backend/src/main/java/com/example/backend/controllers/UserController.java
+++ backend/src/main/java/com/example/backend/controllers/UserController.java
@@ -19,35 +19,9 @@ public class UserController {
     }
 
     @RequestMapping(value = "/updateUser", method = RequestMethod.PUT)
-    public void updateUser(Principal principal,
+    public User updateUser(Principal principal,
                            @RequestBody UpdateUserDTO updateUserDTO) throws Exception {
-        User user = userService.getActivatedUserFromPrincipal(principal);
-        if(user == null){
-            throw new Exception("User not found");
-        }
 
-        if(updateUserDTO.getDrzava() != null){
-            user.setDrzava(updateUserDTO.getDrzava());
-        }
-        if(updateUserDTO.getGrad() != null){
-            user.setGrad(updateUserDTO.getGrad());
-        }
-        if(updateUserDTO.getUsername() != null){
-            user.setUsername(updateUserDTO.getUsername());
-        }
-        if(updateUserDTO.getPassword() != null){
-            user.setPassword(updateUserDTO.getPassword());
-        }
-        if(updateUserDTO.getIme() != null){
-            user.setIme(updateUserDTO.getIme());
-        }
-        if(updateUserDTO.getPrezime() != null){
-            user.setPrezime(updateUserDTO.getPrezime());
-        }
-        if(updateUserDTO.getPhone() != null){
-            user.setPhone(updateUserDTO.getPhone());
-        }
-        userService.save(user);
-
+        return userService.updateUser(principal, updateUserDTO);
     }
 }
